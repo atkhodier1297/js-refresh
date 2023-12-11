ea8739fb40979e5c203ca7f0fa470b7581994f4d
Adam Khodier <khodieradam1297@gmail.com>
2023-12-11 13:18:56 -0500

    added win loss

--- rockpaperscissors/index.js
+++ rockpaperscissors/index.js
@@ -1,24 +1,40 @@
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
 function playGame(playerChoice) {
-    // Generate a random choice for the computer
-    const choices = ['rock', 'paper', 'scissors'];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  
-    // Determine the winner
-    let result;
-    if (playerChoice === computerChoice) {
-      result = "It's a tie!";
-    } else if (
-      (playerChoice === 'rock' && computerChoice === 'scissors') ||
-      (playerChoice === 'paper' && computerChoice === 'rock') ||
-      (playerChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-      result = "You win!";
-    } else {
-      result = "You lose! Try again.";
-    }
-  
-    // Display the result
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = `Computer chose ${computerChoice}. ${result}`;
+  // Generate a random choice for the computer
+  const choices = ['rock', 'paper', 'scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+  // Determine the winner
+  let result;
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+    ties++;
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result = "You win!";
+    wins++;
+  } else {
+    result = "You lose! Try again.";
+    losses++;
   }
+
+  // Display the result and update scores
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = `Computer chose ${computerChoice}. ${result}`;
+
+  const winsElement = document.getElementById('wins');
+  const lossesElement = document.getElementById('losses');
+  const tiesElement = document.getElementById('ties');
+
+  winsElement.textContent = wins;
+  lossesElement.textContent = losses;
+  tiesElement.textContent = ties;
+}
+
   
